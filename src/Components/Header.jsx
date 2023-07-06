5e8494c450734808adf20aa2e5f28c4fc915b77c
Denis Monteiro <deniswmonteiro@gmail.com>
2023-07-06 16:47:21 -0300

    Criação do Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as DogsLogo } from "../Assets/dogs.svg"
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <nav>
-                <Link to="/">Página Inicial</Link>
-                <Link to="/login">Login</Link>
+            <nav className={`${styles.nav} container`}>
+                <Link to="/"
+                    className={styles.logo}
+                    aria-label="Dogs - Página Inicial">
+                    <DogsLogo />
+                </Link>
+                <Link to="/login"
+                    className={styles.login}>
+                    Login/Cadastro
+                </Link>
             </nav>
         </header>
     )

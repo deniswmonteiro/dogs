b972d726a45513ebc2b54a025c5941550fc0c33b
Denis Monteiro <deniswmonteiro@gmail.com>
2023-07-10 17:42:18 -0300

    Criação do footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {ReactComponent as DogsIconFooter} from "../Assets/dogs-footer.svg";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            
+            <DogsIconFooter />
+            <p>Dogs. Alguns direitos reservados.</p>
         </footer>
     )
 }

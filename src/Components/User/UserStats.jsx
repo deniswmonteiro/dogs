4a1d1018e371b1923839edc39c264f8fcb776ec5
Denis Monteiro <deniswmonteiro@gmail.com>
2023-07-11 01:27:56 -0300

    Criação da página de estatíticas de visualizações das fotos

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
 import Head from "../Helper/Head";
 
+// Lazy load
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
+
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" description="Página de estatísticas de acessos às fotos." />
-        </div>
-    )
+    const {data, error, loading, request} = useFetch();
+
+    /** Get stats data */
+    React.useEffect(() => {
+        async function getData() {
+            const token = window.localStorage.getItem("token");
+
+            if (token) {
+                const {url, options} = STATS_GET(token);
+                
+                await request(url, options);
+            }
+        }
+
+        getData();
+    }, []);
+
+    if (error) return <Error error={error} />
+    
+    if (loading) return <Loading />
+
+    if (data) {
+        return (
+            <React.Suspense fallback={<div></div>}>
+                <Head title="Estatísticas" description="Página de estatísticas de acessos às fotos." />
+                <UserStatsGraphs data={data} />
+            </React.Suspense>
+        )
+    }
+
+    else return null;
 }
 
 export default UserStats
---
+++ src/Components/User/UserStatsGraphs.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {VictoryPie, VictoryChart, VictoryBar} from "victory";
+import styles from "./UserStatsGraphs.module.css";
+
+const UserStatsGraphs = ({data}) => {
+    const [graph, setGraph] = React.useState([]);
+    const [total, setTotal] = React.useState(0);
+
+    React.useEffect(() => {
+        /** Transform data to graph data format */
+        const graphData = data.map((item) => {
+            return {
+                x: item.title,
+                y: Number(item.views)
+            }
+        })
+
+        setGraph(graphData);
+
+        /** Sum total views */
+        setTotal(data.map(({views}) => Number(views))
+            .reduce((acc, curr) => acc + curr, 0));
+    }, [data]);
+
+    return (
+        <section className={`${styles.graph} animeLeft`}>
+            <div className={`${styles.total} ${styles.graphItem}`}>
+                <p>
+                    Acessos: {total}
+                </p>
+            </div>
+
+            {/* Graphs */}
+            <div className={styles.graphItem}>
+                <VictoryPie data={graph}
+                    innerRadius={50}
+                    padding={{top: 20, right: 80, bottom: 20, left: 80}}
+                    style={{
+                        data: {
+                            fillOpacity: .9,
+                            stroke: "#FFF",
+                            strokeWidth: 2
+                        },
+                        labels: {
+                            fontSize: 14,
+                            fill: "#333"
+                        }
+                    }} />
+            </div>
+            <div className={styles.graphItem}>
+                <VictoryChart>
+                    <VictoryBar data={graph} alignment="start" />
+                </VictoryChart>
+            </div>
+        </section>
+    )
+}
+
+export default UserStatsGraphs
